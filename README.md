3bc1b116ad6bfec41d0b4d5f782f6ee999b426c9
Douglas <doug919@gmail.com>
2016-04-05 03:29:09 -0400

    params

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Santander
+
+Quickstart
+1. create a directory 'data'
+2. put train.csv and test.csv under such directory
+3. run training program with the following command
+
+python santander_train.py -c 0.001,0.003,0.01,0.03,0.1,0.3,1,3,10,100,300 data
+
+
+You can also use this with options below:
+
--- santander_train.py
+++ santander_train.py
@@ -4,6 +4,7 @@ import sys
 import time
 import logging
 import csv
+import argparse
 
 import cPickle as pkl
 import numpy as np
@@ -11,20 +12,36 @@ from sklearn import svm
 from sklearn.preprocessing import StandardScaler
 from sklearn.cross_validation import KFold
 
-loglevel = logging.DEBUG
-logging.basicConfig(format='[%(levelname)s][%(name)s] %(message)s', level=loglevel)
 
-train_file = 'data/train.csv'
-test_file = 'data/test.csv'
+def parse_list(astr):
+    result = set()
+    for part in astr.split(','):
+        result.add(float(part))
+    return sorted(result)
 
-output_conf_score = 'conf_score.pkl'
-output_predict = 'predict.pkl'
-output_model = 'model.pkl'
+def get_arguments(argv):
+    parser = argparse.ArgumentParser(description='SVM training for Santander')
+    parser.add_argument('input_folder', metavar='INPUT_FOLDER', 
+                        help='data folder')
+    parser.add_argument('-k', '--kfold', metavar='NFOLD', type=int, default=5, 
+                        help='k for kfold cross-validtion. If the value less than 2, we skip the cross-validation and choose the first parameter of -c (DEFAULT: 5)')
+    parser.add_argument('-m', '--output_model', metavar='OUTPUT_MODEL', default='model.pkl', 
+                        help='output, model file name (DEFAULT: model.pkl)')
+    parser.add_argument('-p', '--output_predict', metavar='OUTPUT_PREDICT', default='predict.pkl', 
+                        help='output, predict file name (DEFAULT: predict.pkl)')
+    parser.add_argument('-f', '--output_confidence', metavar='OUTPUT_CONFIDENCE', default='confidence.pkl', 
+                        help='output, confidence-level file name (DEFAULT: confidence.pkl)')
+    parser.add_argument('-c', '--Cs', metavar='C', type=parse_list, default=[1.0], 
+                        help='SVM parameter (DEFAULT: 1). This can be a list expression, e.g., 0.1,1,10,100')
+    
+    parser.add_argument('-v', '--verbose', action='store_true', default=False, 
+                        help='show messages')
+    parser.add_argument('-d', '--debug', action='store_true', default=False, 
+                        help='show debug messages')
+    args = parser.parse_args(argv)
+    return args
 
-Cs = [0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10, 100, 300]
-
-
-def my_read_file(fname, is_train=True):
+def my_read_file(fname, logger, is_train=True):
     with open(fname, 'r') as fr:
         n_rows = len(fr.readlines())
 
@@ -35,13 +52,13 @@ def my_read_file(fname, is_train=True):
 
     n_examples = n_rows - 1
     dim = n_cols - 1 if is_train else n_cols
-    logging.debug('n_cols=%d' % n_cols)
+    logger.debug('n_cols=%d' % n_cols)
 
 
     X_ret = np.zeros((n_examples, dim))
     y_ret = np.zeros(n_examples)
 
-    logging.info('X_shape = %s' % str(X_ret.shape))
+    logger.info('X_shape = %s' % str(X_ret.shape))
 
     with open(train_file, 'r') as fr:
         csvreader = csv.reader(fr, delimiter=',')
@@ -58,15 +75,30 @@ def my_read_file(fname, is_train=True):
                     
             row_idx += 1
 
-    logging.info('number of positives = %d' % (sum(y_ret)))
+    logger.info('number of positives = %d' % (sum(y_ret)))
     return X_ret, y_ret
 
 
 if __name__ == '__main__':
 
+    args = get_arguments(sys.argv[1:])
+
+    if args.debug:
+        loglevel = logging.DEBUG
+    elif args.verbose:
+        loglevel = logging.INFO
+    else:
+        loglevel = logging.ERROR
+    logging.basicConfig(format='[%(levelname)s][%(name)s] %(message)s', level=loglevel) 
+    logger = logging.getLogger(__name__)
+
+    train_file = os.path.join(args.input_folder, 'train.csv')
+    test_file = os.path.join(args.input_folder, 'test.csv')
+
+
     # read data
-    X_train, y_train = my_read_file(train_file, is_train=True)
-    X_test, y_test = my_read_file(test_file, is_train=False)
+    X_train, y_train = my_read_file(train_file, logger, is_train=True)
+    X_test, y_test = my_read_file(test_file, logger, is_train=False)
 
     # normalization
     scaler = StandardScaler(with_mean=True, with_std=True)
@@ -77,36 +109,41 @@ if __name__ == '__main__':
     kfolder = KFold(n=X_train.shape[0], n_folds=5, shuffle=True)
     best_c = 0
     best_score = 0
-
-    for c in Cs:
+    logger.info('cross validation with C = %s' %(str(args.Cs)))
+    for c in args.Cs:
         sum_score = 0.0
         for (i, (train_index, dev_index)) in enumerate(kfolder):
-            logging.info("cross-validation fold %d: train=%d, test=%d" % (i, len(train_index), len(dev_index)))
+            start_t = time.time()
+            logger.info("cross-validation fold %d: train=%d, test=%d" % (i, len(train_index), len(dev_index)))
             X_train_sub, X_dev_sub, y_train_sub, y_dev_sub = X_train[train_index], X_train[dev_index], y_train[train_index], y_train[dev_index]
             classifier = svm.LinearSVC(C=c)
             classifier.fit(X_train_sub, y_train_sub)
             score = classifier.score(X_dev_sub, y_dev_sub)
-            
-            logging.debug('score = %.5f' % (score))
+            logger.info('training time: %.3f seconds' % (time.time()-start_t))
+
+            logger.debug('score = %.5f' % (score))
             sum_score += score
 
         mean_score = sum_score/len(kfolder)
-        logging.info('C = %f, mean_score = %f' % (c, mean_score))
+        logger.info('C = %f, mean_score = %f' % (c, mean_score))
         if mean_score > best_score:
-            logging.info('BEST SCORE!!!')
+            logger.info('BEST SCORE!!!')
             best_c = c
             best_score = mean_score
 
 
     # train with the best C and with all examples
-    logging.info('best_c = %f' % best_c)
+    logger.info('best_c = %f' % best_c)
     classifier = svm.LinearSVC(C=best_c)
     classifier.fit(X_train, y_train)
     conf_score = classifier.decision_function(X_test)
     y_predict = classifier.predict(X_test)
 
     # save results
-    pkl.dump(conf_score, open(output_conf_score, 'wb'))
-    pkl.dump(y_predict, open(output_predict, 'wb'))
-    pkl.dump(classifier, open(output_model, 'wb'))
+    logger.info('dumping %s' % args.output_confidence)
+    pkl.dump(conf_score, open(args.output_confidence, 'wb'))
+    logger.info('dumping %s' % args.output_predict)
+    pkl.dump(y_predict, open(args.output_predict, 'wb'))
+    logger.info('dumping %s' % args.output_model)
+    pkl.dump(classifier, open(args.output_model, 'wb'))
 
